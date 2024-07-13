d6c66b805a852e7a414bd9dacc0d548ca8f7aabc
Rajaram Manikanta <rajarammanikantamani@gmail.com>
2024-07-13 12:14:42 +0530

    Invoice module updated

--- src/components/InvoiceForm/index.js
+++ src/components/InvoiceForm/index.js
@@ -24,7 +24,7 @@ class InvoiceForm extends Component {
 
   async componentDidMount() {
     try {
-      const response = await axios.get('https://sngrbackend.onrender.com/api/invoices/latest-page');
+      const response = await axios.get('https://sngrbackend.onrender.com/api/invoices');
       const latestPageNumber = response.data.page || 0; // If no page number is found, start from 0
       this.setState({ page: latestPageNumber + 1 });
     } catch (error) {
@@ -83,12 +83,7 @@ class InvoiceForm extends Component {
       // Show popup with total estimation bill
       this.setState({ showPopup: true });
 
-      // Get the latest page number from the API and update the state
-      const pageResponse = await axios.get('https://sngrbackend.onrender.com/api/invoices/latest-page');
-      const latestPageNumber = pageResponse.data.page || 0;
-      const updatedPage = latestPageNumber + 1;
-
-      // Reset form fields and update the page number
+      const updatedPage = this.state.page + 1;
       this.setState({
         page: updatedPage,
         invoiceDate: '',
@@ -99,7 +94,7 @@ class InvoiceForm extends Component {
         sofaModel: '',
         seaterType: '', // Reset seater type
         fabricCharge: '',
-        totalEstimationBill: '',
+
         sofaSeatingCharge:'',
         hrFoamSeatingCharge:'',
         coirFoamSeatingCharge:'',
@@ -111,7 +106,7 @@ class InvoiceForm extends Component {
   };
 
   closePopup = () => {
-    this.setState({ showPopup: false });
+    this.setState({ showPopup: false ,  totalEstimationBill: '',});
   };
 
   render() {
@@ -280,9 +275,10 @@ class InvoiceForm extends Component {
               <div className="popup-content">
                 <span className="close-popup" onClick={this.closePopup}>&times;</span>
                 <h3>Invoice Generated Successfully!</h3>
-                <p>Total Estimated Bill: ${totalEstimationBill}</p>
+                <p>Total Estimated Bill: ₹{totalEstimationBill}</p>
                 <div className="popup-buttons">
-                  <button className="popup-button" onClick={() => alert('Navigate to customers page')}>Go to Customers Page</button>
+                  <a href="/customers">    <button className="popup-button" href="/customers">Go to Customers Page</button></a>
+              
                   <button className="popup-button" onClick={this.closePopup}>Close</button>
                 </div>
               </div>
